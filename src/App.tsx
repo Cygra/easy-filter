5dd7c6c958dda28389db79bd998aaf123ba5adf4
Cygra <sjtuwbh@gmail.com>
2019-04-09 22:51:58 +0800

    add drag event
    TODO: handle multi-files and no-image files situation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React, { Component, ChangeEvent } from 'react';
+import React, { Component, ChangeEvent, DragEvent } from 'react';
 import './App.css';
 
 interface Props {
 
+}
+
+enum FileStatus {
+  DRAG_ENTER,
+  NULL,
+  UPLOADED,
 }
 
 interface State {
@@ -17,6 +23,7 @@ interface State {
   brightness: number;
   contrast: number;
   dropShadow: [number, number, number, string];
+  dragStatus: FileStatus;
 }
 
 class App extends Component<Props, State> {
@@ -34,17 +41,54 @@ class App extends Component<Props, State> {
       opacity: 100,
       brightness: 100,
       contrast: 100,
-      dropShadow: [0, 0, 0, '#000']
+      dropShadow: [0, 0, 0, '#000'],
+      dragStatus: FileStatus.NULL,
     };
     this.reader = new FileReader();
     this.reader.onload = () => this.setState({
+      dragStatus: FileStatus.UPLOADED,
       imgUrl: String(this.reader.result),
-    })
+    });
   }
 
   onImgChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const files: FileList | null = e.target.files;
+    this.handleFile(files);
+  }
+
+  onDragEnter = (e: DragEvent<HTMLLabelElement>): void => {
+    e.preventDefault();
+    this.setState({ dragStatus: FileStatus.DRAG_ENTER });
+  }
+
+  onDragOver = (e: DragEvent<HTMLLabelElement>): void => {
+    e.preventDefault();
+  }
+
+  onDragLeave = () => {
+    !this.state.imgUrl && this.setState({ dragStatus: FileStatus.NULL });
+  }
+
+  dropImage = (e: DragEvent<HTMLLabelElement>): void => {
+    e.preventDefault();
+    const files: FileList | null = e.dataTransfer.files;
+    this.handleFile(files);
+  }
+
+  handleFile = (files: FileList | null): void => {
     files && this.reader!.readAsDataURL(files[0]);
+  }
+
+  get labelText(): string {
+    switch (this.state.dragStatus) {
+      case FileStatus.DRAG_ENTER:
+        return 'Drop to upload!';
+      case FileStatus.UPLOADED:
+        return 'File has been uploaded!';
+      case FileStatus.NULL:
+      default:
+        return 'Drop or click to upload!';
+    }
   }
 
   render() {
@@ -61,12 +105,16 @@ class App extends Component<Props, State> {
       hueRotate,
       dropShadow,
     } = this.state;
-    const [ offX, offY, blurRad, color ] = dropShadow;
-    console.log(color)
+    const [offX, offY, blurRad, color] = dropShadow;
     return (
       <div className="App">
-        <label htmlFor="upload-file">
-          Click here to upload!
+        <label
+          htmlFor="upload-file"
+          onDragEnter={this.onDragEnter}
+          onDragOver={e => e.preventDefault()}
+          onDragLeave={this.onDragLeave}
+          onDrop={this.dropImage}>
+          {this.labelText}
         </label>
         <input
           id="upload-file"
